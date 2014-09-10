48db77b513f4f3856be9a431d491d578e43dc0b1
Konstantin Goncharik <botankras@gmail.com>
2014-09-11 00:40:47 +0800

    OCDFAInspection

--- main.cpp
+++ main.cpp
@@ -45,7 +45,20 @@ int main() {
     cout << "sa1 " << sa1.getA() << endl;
     cout << endl;
 
-    cout << (a >= b) << endl; //crash
+    int i = 0;
+    int brk = 10;
+    for(;;) {
+        if (brk) { //change this expression leads to a change in the inspection below (e.g. "if (true)" )
+            if(i == 0) { // inspection depends of the level of nesting
+                cout << "i==0 is " << ((i == 0) ? "true" : "false") << endl;
+            }
+        }
+        ++i;
+        if (i > brk) break;
+        cout << i << endl;
+    }
+
+    cout << (a >= b) << endl;
     cout << Clazz(42).getIntValue() << endl;
     return 0;
 }
