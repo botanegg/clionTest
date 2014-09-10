bae84e27bdeb3b3818215c9432f4183af08ccab7
Konstantin Goncharik <botankras@gmail.com>
2014-09-10 21:25:10 +0800

    Generator add additional public blocks

--- main.cpp
+++ main.cpp
@@ -6,6 +6,19 @@ class Clazz {
 private:
     int intValue;
 
+public: // additional block created after CLion generation getter/setter
+    int getIntValue() const {
+        return intValue;
+    }
+
+    void setIntValue(int intValue) {
+        Clazz::intValue = intValue;
+    }
+
+public: // additional block created after CLion generation constructor
+    Clazz(int intValue) : intValue(intValue) {
+    }
+
 public:
 
     bool operator<(const Clazz &other) const {
@@ -20,6 +33,6 @@ public:
 
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << Clazz(42).getIntValue() << endl;
     return 0;
 }
